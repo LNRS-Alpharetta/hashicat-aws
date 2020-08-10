d50b934c96d7526153001cb9d0773218a47572a6
Jack Coleman <jack.coleman@lexisnexisrisk.com>
2020-08-10 13:52:53 -0400

    Update variables.tf
    add cidr variable

--- variables.tf
+++ variables.tf
@@ -19,6 +19,11 @@ variable "address_space" {
   default     = "10.0.0.0/16"
 }
 
+variable "cidr" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+
 variable "subnet_prefix" {
   description = "The address prefix to use for the subnet."
   default     = "10.0.10.0/24"
